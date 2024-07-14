636986aa39b1a73f5addf60496f63222eb854b26
flooijdt <flooijdt@posteo.de>
2024-07-14 19:21:24 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn make_looper(string: &str) -> impl FnMut() -> char + '_ {
+    fn string.into() -> &str {
+        string.chars().next()
+    }
+}
+
+fn main(){
+    let mut abc = make_looper("abc");
+    println!("{:?}", abc());
 }
