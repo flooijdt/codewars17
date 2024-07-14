507236f805a09920f82be408e16e4346a30274cd
flooijdt <flooijdt@posteo.de>
2024-07-14 19:53:04 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::IndexMut;
+
 // Summary: Write a function which takes an array data of numbers and returns the largest difference in indexes j - i such that data[i] <= data[j]
 fn main() {
     largest_difference(&[9, 4, 1, 10, 3, 4, 0, -1, -2]);
@@ -5,7 +7,17 @@ fn main() {
 
 fn largest_difference(data: &[i32]) /*-> usize */
 {
+    let mut dif: i32 = 0;
+
     for i in data {
+        for j in data {
+            if i <= j {
+                println!(
+                    "{:?}",
+                    data.iter_mut().position(j).unwrap() - data.iter_mut().position(i).unwrap()
+                );
+            }
+        }
         println!("{:?}", &i);
     }
 }
