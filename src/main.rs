383f5f4ce43bcf6762bc3f74510c73ecd003c3ec
flooijdt <flooijdt@posteo.de>
2024-07-14 19:35:30 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Summary: Write a function which takes an array data of numbers and returns the largest difference in indexes j - i such that data[i] <= data[j]
 fn main() {
-    largest_difference([9, 4, 1, 10, 3, 4, 0, -1, -2]);
+    largest_difference(&[9, 4, 1, 10, 3, 4, 0, -1, -2]);
 }
 
 fn largest_difference(data: &[i32]) /*-> usize */
