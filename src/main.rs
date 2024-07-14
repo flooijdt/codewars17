045f763165e6f41ad4856b957577a000bb5e79cf
flooijdt <flooijdt@posteo.de>
2024-07-14 19:35:09 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     largest_difference([9, 4, 1, 10, 3, 4, 0, -1, -2]);
 }
 
-fn largest_difference(data: &[i32]) -> usize {}
+fn largest_difference(data: &[i32]) /*-> usize */
+{
+    for i in data {
+        println!("{:?}", &i);
+    }
+}
