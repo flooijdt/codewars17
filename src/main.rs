d3ada456a2aa663c2a436c0da5b73ae240d31d33
flooijdt <flooijdt@posteo.de>
2024-07-15 09:20:10 -0300

    default commit.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 // Summary: Write a function which takes an array data of numbers and returns the largest difference in indexes j - i such that data[i] <= data[j]
 fn main() {
-    largest_difference(&[9, 4, 1, 10, 3, 4, 0, -1, -2]);
+    println!("{:?}", largest_difference(&[9, 4, 1, 10, 3, 4, 0, -1, -2]));
 }
 
 fn largest_difference(data: &[i32]) -> usize {
+    let mut ini = 0;
+
     for (i, item) in data.iter().enumerate() {
         for (j, aitem) in data.iter().enumerate() {
-            if item <= aitem {
-                return j - i + 1;
+            if item <= aitem && j > i && j - i > ini {
+                ini = j - i;
             }
         }
     }
-    0
+    ini
 }
